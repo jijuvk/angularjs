0dc0d2e23dece6cc61a81746c5e5d6607da74eca
jijuvk <jijuvk@gmail.com>
2014-06-20 12:46:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var myApp = angular.module('myApp', []);
+
 myApp.factory('Data',function(){
   return {message : "returning from service"}
+})
+
+
+myApp.filter('reverse',function (Data1){
+  return function(text){
+    return text.split("").reverse().join("")+Data1.message;
+  }
 })
 
 myApp.factory('Data1',function(){
   return {message : "returning from second service"}
 })
+
+
 function FirstCtrl($scope,Data){
   $scope.data = Data;
   
@@ -14,10 +24,14 @@ function FirstCtrl($scope,Data){
   }
 }
 
+
+
 function SecondCtrl($scope,Data1){
  $scope.data = Data1;
+ 
  $scope.reverseMessage =function(message){
 return message.split("").reverse().join("") +" REVERSED";
+
 
  }
 }
